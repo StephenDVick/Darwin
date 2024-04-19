06df98db8631a9c3414f4c4adc9eddafca510998
Stephen Vick <stephen.vick@soltech.net>
2024-04-19 11:45:55 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Darwin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InterruptButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = @".\Sounds\01.wav";
+            player.Load();
+            player.Play();
+        }
+    }
+}
