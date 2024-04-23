f307ae1afaaf7c5efddfddeb96a8fd3cac54da2a
Stephen Vick <stephen.vick@soltech.net>
2024-04-23 09:18:23 -0400

    Added end the meeting wav

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,12 +13,28 @@ namespace Darwin
         public MainWindow()
         {
             InitializeComponent();
+
+            // Set the window startup location to manual
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.Manual;
+
+            // Subscribe to the Loaded event
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get the working area of the screen
+            var workingArea = System.Windows.SystemParameters.WorkArea;
+
+            // Set the window position to the bottom right corner
+            this.Left = workingArea.Right - this.Width;
+            this.Top = workingArea.Bottom - this.Height;
         }
 
         private void InterruptButton_Click(object sender, RoutedEventArgs e)
         {
             System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            player.SoundLocation = @".\Sounds\02.wav";
+            player.SoundLocation = @".\Sounds\03.wav";
             player.Load();
             player.Play();
         }
